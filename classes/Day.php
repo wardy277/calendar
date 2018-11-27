2f17b64ff54208702839905218681997fb13dcde
Chris Ward <wardy277@gmail.com>
2018-11-27 13:58:56 +0000

    potentialy fix slowdown by caching get shows

--- classes/Day.php
+++ classes/Day.php
@@ -8,6 +8,8 @@
  */
 class Day extends Entity{
 
+	protected static $_cache = false;
+
 	public function __construct($date){
 		$this->_data['date'] = $date;
 	}
@@ -17,6 +19,7 @@ class Day extends Entity{
 	 */
 	public function getTorrents(){
 		global $db, $session;
+
 
 		//work out timezones - convert user's local timezone to GMT as that's what the db is set to
 		$time_zone = $session->getTimezone();
@@ -40,7 +43,7 @@ class Day extends Entity{
 		$start_range = $start_date->format('Y-m-d H:i:s');
 		$end_range   = $end_date->format('Y-m-d H:i:s');
 
-
+/*
 		$sql = $db->build("SELECT t.title AS show_name, e.title, UNIX_TIMESTAMP(e.aired_date) AS time, season, episode
 							FROM tv_shows t
 							JOIN episode_list e ON e.show_id = t.id
@@ -50,15 +53,19 @@ class Day extends Entity{
 							AND episode > 0
 							ORDER BY time, title, season, episode",
 			$session->getUserId(), $start_range, $end_range);
-
 		$data  = $db->getArray($sql);
+*/
+
 		$shows = array();
+		$data = $this->getShows($start_date->format('Y-m-d'));
 
 		if(!empty($data)){
 			foreach($data as $row){
 				$shows[] = new Zooqle($row);
 			}
 		}
+
+		//return $shows;
 
 		//special -- dc week
 		if($this->getDate()->format('D') == "Thu"){
@@ -70,4 +77,40 @@ class Day extends Entity{
 		return $shows;
 	}
 
+
+	protected function getShows($day){
+		if(static::$_cache === false){
+			global $db, $session;
+
+			$date = new DateTime($day);
+
+			$date->modify('first day of last month');
+			$start_range = $date->format('Y-m-d')." 00:00:00";
+
+                        $date->modify('last day of next month');
+                        $date->modify('last day of next month');
+			$end_range = $date->format('Y-m-d')." 23:59:59";
+
+
+			//recache
+			$sql = $db->build("SELECT date(e.aired_date) as day, t.title AS show_name, e.title, UNIX_TIMESTAMP(e.aired_date) AS time, season, episode
+                                                        FROM tv_shows t
+                                                        JOIN episode_list e ON e.show_id = t.id
+                                                        JOIN users_shows u ON u.show_id = t.id
+                                                        WHERE u.user_id = '?'
+                                                        AND e.aired_date BETWEEN '?' AND '?'
+                                                        AND episode > 0
+							
+							#group by date(e.aired_date)
+                                                        ORDER BY time, title, season, episode",
+	                        $session->getUserId(), $start_range, $end_range);
+
+			foreach($db->getArray($sql) as $row){
+				static::$_cache[$row['day']][] = $row;
+			}
+		}
+
+
+		return static::$_cache[$day];
+	}
 }
